f019c6ecb8128754bac552e4aeaae8ea13e584dc
kuuonkuuon <phamnguyenthienan8.1@gmail.com>
2026-06-18 13:25:14 +0700

    api temp

---
+++ src/api/apiClient.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default apiClient;
